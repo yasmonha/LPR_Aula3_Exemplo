49e3e37afb9ca14060be46c975883b6efd34be53
yasmonha <yasmim.ribeiro5005@gmail.com>
2025-03-17 10:24:31 -0300

    felizaniversario

--- cloneGit.cpp
+++ cloneGit.cpp
@@ -7,10 +7,10 @@ int main() {
     setlocale(LC_ALL, "pt_BR.utf8"); //Para usar acentuação
     cout << "\033[1;34m***************************************************\033[0m\n";
     cout << "\033[1;32m*                                                 *\033[0m\n";
-    cout << "\033[1;32m*   \033[1;33m PARABÉNS 34DS!                     \033[1;32m          *\033[0m\n";
+    cout << "\033[1;32m*   \033[1;33m FELIZ ANIVERSÁRIO!                     \033[1;32m          *\033[0m\n";
     cout << "\033[1;32m*                                                 *\033[0m\n";
-    cout << "\033[1;32m*   \033[1;36mVocê clonou um repositório público do Git     \033[1;32m*\033[0m\n";
-    cout << "\033[1;32m*   \033[1;36mpara o seu repositório local!                 \033[1;32m*\033[0m\n";
+    cout << "\033[1;32m*   \033[1;36mFeliz Aniversário! Que a felicidade acompanhe sempre    \033[1;32m*\033[0m\n";
+    cout << "\033[1;32m*   \033[1;36mvocê, não só hoje, mas todos os dias da sua vida.!                 \033[1;32m*\033[0m\n";
     cout << "\033[1;32m*                                                 *\033[0m\n";
     cout << "\033[1;34m***************************************************\033[0m\n";
 
